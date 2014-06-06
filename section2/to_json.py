1487f129e6a22cfccd560f8cafb53d5f7c542731
Swizec Teller <swizec@swizec.com>
2014-06-06 07:25:51 -0700

    converted raw data to json

--- section2/to_json.py
+++ section2/to_json.py
@@ -1,10 +1,18 @@
 
+import json
 from pyquery import PyQuery as pq
-from bs4 import BeautifulSoup
+
+CELL_NAMES = ["time", "city", "state", "shape", "duration", "summary", "posted"]
+
+def parse_one(row):
+    return dict(zip(CELL_NAMES, [pq(td).text() for td in row.find("td")]))
 
 def to_json(path):
     d = pq(filename=path)
-    print d("tr").size()
+    d("tr:first").remove()
+
+    data = [parse_one(pq(tr)) for tr in d("tr")]
+    open("triangle_ufos.json", "w").write(json.dumps(data))
 
 if __name__ == "__main__":
     print to_json("./raw.html")
