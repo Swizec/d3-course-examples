90c3e86cb7c75c9e952e928a6c2d2eba2f4e2d66
Swizec Teller <swizec@swizec.com>
2014-06-18 11:11:00 -0700

    managed to parse the html

--- section3/code.js
+++ section3/code.js
@@ -6,9 +6,19 @@
 	    .style({width: "100%",
 		    height: 1024});
 
-    d3.html("http://www.nuforc.org/webreports/ndxpost.html",
-	    function (data) {
-		console.log(data);
-	    });
+    d3.html("time-index.html",
+	    function (fragment) {
+		var formatter = d3.time.format("%x"),
+                    data = Array.prototype.map.call(
+		    fragment.lastElementChild.querySelectorAll("tbody tr"),
+		    function (row) {
+			var a = row.querySelector("a");
 
+			return {time: formatter.parse(a.textContent),
+				count: Number(row.querySelectorAll("td")[1].textContent),
+				link: a.href};
+		    });
+                
+                console.log(a.href);
+	    });
 })();
