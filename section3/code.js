ce2fdbe1e1e63500cec42172cf8d8fce941b6c88
Swizec Teller <swizec@swizec.com>
2014-06-19 10:49:27 -0700

    parsing data for single state

--- section3/code.js
+++ section3/code.js
@@ -6,19 +6,60 @@
 	    .style({width: "100%",
 		    height: 1024});
 
-    d3.html("time-index.html",
-	    function (fragment) {
-		var formatter = d3.time.format("%x"),
-                    data = Array.prototype.map.call(
-		    fragment.lastElementChild.querySelectorAll("tbody tr"),
-		    function (row) {
-			var a = row.querySelector("a");
+    var slugify = function (string) {
+        return string
+            .toLowerCase()
+            .replace(/ /g,'-')
+            .replace(/[^\w-]+/g,'-');
+    };
 
-			return {time: formatter.parse(a.textContent),
-				count: Number(row.querySelectorAll("td")[1].textContent),
-				link: a.href};
-		    });
+    var parse_index = function (fragment) {
+	var formatter = d3.time.format("%x");
+
+        return Array.prototype.map.call(
+	    fragment.querySelectorAll("tbody tr"),
+	    function (row) {
+	        var a = row.querySelector("a");
                 
-                console.log(data);
+                return {state: a.textContent,
+                        count: Number(row.querySelectorAll("td")[1].textContent),
+                        link: "data/"+slugify(a.textContent)+".html"};
+                
 	    });
+    };
+
+    var fetch_a_state = function (state, callback) {
+        callback || (callback = function () {});
+
+        d3.html(state.link, function (fragment) {
+            var time_formatter = d3.time.format("%x %H:%M"),
+                posted_formatter = d3.time.format("%x");
+
+            var data = Array.prototype.map.call(
+                fragment.querySelectorAll("tbody tr"),
+                function (row) {
+                    var cells = row.querySelectorAll("td"),
+                        parsed = {};
+
+                    ["time", "city", "state", "shape", 
+                     "duration", "summary", "posted"].forEach(
+                        function (key, i) {
+                            parsed[key] = cells[i].textContent;
+                        });
+                    
+                    parsed.time = time_formatter.parse(parsed.time);
+                    parsed.posted = posted_formatter.parse(parsed.posted);
+
+                    return parsed;
+                });
+
+            console.log(data[0]);
+        });
+    };
+
+    d3.html("state-index.html", function (fragment) {
+        var index = parse_index(fragment);
+                
+        fetch_a_state(index[0]);
+    });
 })();
