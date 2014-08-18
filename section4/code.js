a0f84e1f7b9bfa2b8773b2432d0b4a72640453b3
Swizec Teller <swizec@swizec.com>
2014-08-18 11:15:29 -0700

    rendering state borders

---
+++ section4/code.js
@@ -0,0 +1,27 @@
+
+(function () {
+
+    var width = 960,
+        height = 600;
+
+    var projection = d3.geo.albersUsa()
+            .scale(1280)
+            .translate([width / 2, height / 2]),
+        path = d3.geo.path()
+            .projection(projection);
+
+    var svg = d3.select("#graph").append("svg")
+            .attr("width", width)
+            .attr("height", height);
+
+    queue()
+        .defer(d3.json, "us.json")
+        .await(function (err, US) {
+            svg.append("path")
+                .datum(topojson.mesh(US, US.objects.states, 
+                                     function(a, b) { return a !== b; }))
+                .attr("class", "states")
+                .attr("d", path);
+        });
+
+})();
