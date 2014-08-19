74eb0b5c98f1a0a3c46f66823b801166d446ae08
Swizec Teller <swizec@swizec.com>
2014-08-18 23:55:37 -0700

    I messed up, id's don't match

--- section4/code.js
+++ section4/code.js
@@ -14,6 +14,56 @@
             .attr("width", width)
             .attr("height", height);
 
+    var stateIdMap = d3.map(_.zipObject([
+        [1, "AL"],
+        [2, "AK"],
+        [4, "AZ"],
+        [5, "AR"],
+        [6, "CA"],
+        [8, "CO"],
+        [9, "CT"],
+        [10, "DE"],
+        [12, "FL"],
+        [13, "GA"],
+        [16, "ID"],
+        [18, "IN"],
+        [19, "IA"],
+        [20, "KS"],
+        [21, "KY"],
+        [22, "LA"],
+        [23, "ME"],
+        [24, "MD"],
+        [25, "MA"],
+        [26, "MI"],
+        [27, "MN"],
+        [28, "MS"]
+        [29, "MO"],
+        [30, "MT"],
+        [31, "NE"],
+        [32, "NV"],
+        [33, "NH"],
+        [34, "NJ"],
+        [35, "NM"],
+        [36, "NY"],
+        [37, "NC"],
+        [38, "ND"],
+        [39, "OH"],
+        [40, "OK"],
+        [41, "OR"],
+        [42, "PA"],
+        [44, "RI"],
+        [45, "SC"],
+        [46, "SD"],
+        [47, "TN"],
+        [48, "TX"],
+        [49, "UT"],
+        [50, "VT"],
+        [51, "VA"],
+        [53, "WA"],
+        [54, "WV"],
+        [56, "WY"]
+    ]));
+
     queue()
         .defer(d3.json, "us.json")
         .defer(d3.csv, "full-data.csv")
@@ -21,7 +71,8 @@
             ufos = _.groupBy(ufos.filter(function (ufo) { return !!ufo.state; }),
                              function (ufo) { return ufo.state; });
 
-            console.log(US);
+            console.log(_.sortBy(_.pluck(US.objects.states.geometries, "id")));
+            console.log(_.sortBy(stateIdMap.keys(), Number));
 
             svg.append("g")
                 .attr("class", "states")
@@ -29,7 +80,10 @@
                 .data(topojson.feature(US, US.objects.states).features)
                 .enter().append("path")
                 //.attr("class", function(d) { return quantize(rateById.get(d.id)); })
-                .attr("d", path);
+                .attr("d", path)
+                .on("click", function (d) {
+                    console.log(d.id);
+                });
 
             svg.append("path")
                 .datum(topojson.mesh(US, US.objects.states, 
