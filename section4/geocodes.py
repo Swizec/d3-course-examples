350270704cb84c15cdf495d66af5ed6c2c39ed1d
Swizec Teller <swizec@swizec.com>
2014-08-25 03:50:51 -0700

    let's try bing

--- section4/geocodes.py
+++ section4/geocodes.py
@@ -1,26 +1,32 @@
 
 import csv, gmaps
 #from geocode import latlon, NoResultError
-from gmaps import Geocoding
+#from gmaps import Geocoding
+#from omgeo import Geocoder
+from geopy.geocoders import Bing
 
 #maps = Geocoding(api_key="AIzaSyB9lcJWc0pHQk89WF_MJ6OcyttOqx0t4LE")
-maps = Geocoding(api_key="AIzaSyBfpW7AtJNiLd1xCY6_Vh5Q7BS2d9wARgA")
+#maps = Geocoding(api_key="AIzaSyBfpW7AtJNiLd1xCY6_Vh5Q7BS2d9wARgA")
+#bing = omgeo.services.Bing(settings={"api_key": "AtvTgjJ5FOjkgB5I291TXiDZFVZ7XK2_o4mqIBekDtcg0tDSBqmJpfjJckYqd2la"})
+#geocoder = Geocoder(sources=[['bing', {}],])
+
+bing = Bing("AtvTgjJ5FOjkgB5I291TXiDZFVZ7XK2_o4mqIBekDtcg0tDSBqmJpfjJckYqd2la")
 
 STATES = ["AK", "AL", "AR", "AZ", "CA", "CO", "CT", "DC", "DE", "FL", "GA", "HI", "IA", "ID", "IL", "IN", "KS", "KY", "LA", "MA", "MD", "ME", "MI", "MN", "MO", "MS", "MT", "NC", "ND", "NE", "NH", "NJ", "NM", "NV", "NY", "OH", "OK", "OR", "PA", "RI", "SC", "SD", "TN", "TX", "UT", "VA", "VT", "WA", "WI", "WV", "WY"]
 
-LAST = """09/01/2010 06:45,Garden Grove,CA,Sphere,10 seconds,I witnessed to UFO's collide,04/18/2012,33.7739053,-117.9414477""".split(",")
+#LAST = """09/01/2010 06:45,Garden Grove,CA,Sphere,10 seconds,I witnessed to UFO's collide,04/18/2012,33.7739053,-117.9414477""".split(",")
 
 def fetch():
     with open("data/full-data.csv") as csvin:
         reader = csv.reader(csvin)
 
-        with open("data/full-data-geodata.csv", "ab") as csvout:
+        with open("data/full-data-geodata.csv", "wb") as csvout:
             writer = csv.writer(csvout)
 
             head = reader.next()
             head += ['lat', 'lon']
 
-            #writer.writerow(head)
+            writer.writerow(head)
 
             candidates = (row for row in reader if row[2] != "" and row[2] in STATES)
 
@@ -29,13 +35,17 @@ def fetch():
                     break
 
             for row in candidates:
-                try:
-                    row += [v for (k, v) in
-                            maps.geocode(row[1])[0]['geometry']['location'].items()]
-                    print row
-                    writer.writerow(row)
-                except gmaps.errors.NoResults:
-                    pass
+                loc = bing.geocode(row[1])
+                row += [loc.latitude, loc.longitude]
+
+                writer.writerow(row)
+                # try:
+                #     row += [v for (k, v) in
+                #             maps.geocode(row[1])[0]['geometry']['location'].items()]
+                #     print row
+                #     writer.writerow(row)
+                # except gmaps.errors.NoResults:
+                #     pass
 
 if __name__ == "__main__":
     fetch()
