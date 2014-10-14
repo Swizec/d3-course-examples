915ce36e6b93dce6905e1d27851479b7397397c6
Swizec Teller <swizec@swizec.com>
2014-10-14 15:37:36 +0200

    even more code organisation

--- section5/code.js
+++ section5/code.js
@@ -26,55 +26,18 @@
             var ufos = prepare.ufos(_ufos);
             populations = prepare.populations(populations);
 
-            var drawers = Drawers(svg, ufos, populations);
-
-            drawers.map(US, geo_path);
-
+            
             var tmp = clustered_ufos(_ufos, geo_projection),
                 clustered = tmp[0],
                 clusters = tmp[1],
 
                 cluster_populations = prepare.cluster_populations(clustered, city_populations);
 
-            var
-                ratios = _.mapValues(clustered,
-                                     function (group, key) {
-                                         var population = cluster_populations[key];
+            var drawers = Drawers(svg, ufos, populations);
 
-                                         if (population == 0) {
-                                             return 0;
-                                         }
-
-                                         return group.length/population;
-                                     }),
-                R = d3.scale.linear()
-                    .domain([0, d3.max(_.values(ratios))])
-                    .range([2, 20]);
-
-            var base_positions = prepare.base_positions(military_bases, geo_projection);
-            
-            svg.append("g")
-                .selectAll("path")
-                .data(base_positions)
-                .enter()
-                .append("path")
-                .attr("d", d3.svg.symbol().type("cross").size(32))
-                .attr("class", "base")
-                .attr("transform", function (d) {
-                    return "translate("+d[0]+","+d[1]+")";
-                });
-
-            svg.append("g")
-                .selectAll("circle")
-                .data(clusters.centroids)
-                .enter()
-                .append("circle")
-                .attr({
-                    cx: function (d) { return d[0]; },
-                    cy: function (d) { return d[1]; },
-                    r: function (d, i) { return R(ratios[i]); },
-                    class: "point"
-                });
+            drawers.map(US, geo_path);
+            drawers.bases(military_bases, geo_projection);
+            drawers.centroids(clusters, clustered, cluster_populations);
 
         });
 
--- section5/drawers.js
+++ section5/drawers.js
@@ -12,11 +12,11 @@ var Drawers = function (svg, ufos, populations) {
                     .domain(d3.extent(_.values(ufoCounts)))
                     .range(d3.range(9).map(function(i) { return "q" + i + "-9-green"; }));
             
-        var states = svg.append("g")
-                .attr("class", "states")
-                .selectAll("path")
-                .data(topojson.feature(US, US.objects.states).features)
-                .enter();
+            var states = svg.append("g")
+                    .attr("class", "states")
+                    .selectAll("path")
+                    .data(topojson.feature(US, US.objects.states).features)
+                    .enter();
             
             states.append("path")
                 .attr("d", geo_path)
@@ -30,6 +30,51 @@ var Drawers = function (svg, ufos, populations) {
                 .attr("class", "borders")
                 .attr("d", geo_path);
             
+        },
+
+        bases: function (military_bases, geo_projection) {
+            var base_positions = prepare.base_positions(military_bases, geo_projection);
+            
+            svg.append("g")
+                .selectAll("path")
+                .data(base_positions)
+                .enter()
+                .append("path")
+                .attr("d", d3.svg.symbol().type("cross").size(32))
+                .attr("class", "base")
+                .attr("transform", function (d) {
+                    return "translate("+d[0]+","+d[1]+")";
+                });
+        },
+
+        centroids: function (clusters, clustered, cluster_populations) {
+            var
+                ratios = _.mapValues(clustered,
+                                     function (group, key) {
+                                         var population = cluster_populations[key];
+
+                                         if (population == 0) {
+                                             return 0;
+                                         }
+
+                                         return group.length/population;
+                                     }),
+                R = d3.scale.linear()
+                    .domain([0, d3.max(_.values(ratios))])
+                    .range([2, 20]);
+            
+            svg.append("g")
+                .selectAll("circle")
+                .data(clusters.centroids)
+                .enter()
+                .append("circle")
+                .attr({
+                    cx: function (d) { return d[0]; },
+                    cy: function (d) { return d[1]; },
+                    r: function (d, i) { return R(ratios[i]); },
+                    class: "point"
+                });
+
         }
     };
 };
