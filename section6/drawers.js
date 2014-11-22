e8157d1ddf4199f30703c1b01078486014c535e8
Swizec Teller <swizec@swizec.com>
2014-11-22 12:17:43 +0100

    removed unneeded code

--- section6/drawers.js
+++ section6/drawers.js
@@ -46,35 +46,10 @@ var Drawers = function (svg, ufos, populations, geo_path, geo_projection) {
                 });
         },
 
-        centroids: function (centroids, clustered, cluster_populations) {
-            var
-                ratios = _.mapValues(clustered,
-                                     function (group, key) {
-                                         var population = cluster_populations[key];
-
-                                         if (population == 0) {
-                                             return 0;
-                                         }
-
-                                         return group.length/population;
-                                     }),
-                R = d3.scale.linear()
-                    .domain([0, d3.max(_.values(ratios))])
-                    .range([2, 20]);
-
-            var ufo_count = _.values(clustered)
-                    .reduce(function (sum, group) {
-                        return sum+group.length;
-                    }, 0);
-
+        centroids: function (centroids) {
             centroids = centroids.map(function (pos, i) {
                 return {x: pos[0],
-                        y: pos[1],
-                        max_R: R(ratios[i]),
-                        all_here: clustered[i].length,
-                        abs_all: ufo_count,
-                        population: cluster_populations[i],
-                        count: 0};
+                        y: pos[1]};
             });
             
             svg.append("g")
@@ -93,160 +68,6 @@ var Drawers = function (svg, ufos, populations, geo_path, geo_projection) {
                     id: function (d, i) { return "centroid-"+i; }
                 });
 
-        },
-
-        place_ufos: function (ufos) {
-            if (!ufos) return;
-
-            var format = d3.time.format("%m/%d/%Y %H:%M");
-
-            ufos = _.sortBy(ufos, function (ufo) { return format.parse(ufo.time); });
-
-            var positions = ufos
-                    .map(function (ufo) {
-                        return geo_projection([Number(ufo.lon), Number(ufo.lat)]);
-                    })
-                    .filter(function (pos) { return !!pos; });
-
-            var fps = 60,
-                per_frame = Math.ceil(positions.length > fps 
-                                      ? positions.length/fps 
-                                      : 1),
-                ufos_in_batch = positions.length;
-
-            // d3.timer((function () {
-            //     var counter = 0,
-            var         previous = (new Date()).getTime();
-
-            //     return function draw () {
-                    var now = new Date().getTime(),
-                        delta = now-previous,
-                        //frames = Math.ceil(delta/(1000/fps))
-                        frames = fps;
-
-                    var to_draw = {
-                        pos: positions.splice(0, per_frame*frames),
-                        ufos: ufos.splice(0, per_frame*frames)
-                    };
-
-            var selector = to_draw.ufos.map(function (ufo) {
-                            return "#centroid-"+ufo.cluster;
-                        }).join(", ");
-
-            console.log("drawing", selector);
-
-                    var // g = svg.append("g")
-                        //     .attr("class", "points")
-                        //     .datum({type: "points"}),
-                        // drawn = g.selectAll("circle")
-                        //     .data(to_draw.pos)
-                        //     .enter()
-                        //     .append("circle")
-                        //     .attr({
-                        //         cx: function (d) { return d[0]; },
-                        //         cy: function (d) { return d[1]; },
-                        //         r: 2,
-                        //         class: "point"
-                        //     }),
-                        centroids = d3.selectAll(selector);
-                                                               
-                    // g.transition()
-                    //     .duration(500)
-                    //     .style("opacity", .3);
-
-                    centroids.each(function (d) {
-                        d.count += 1;
-                        d3.select(this).datum(d);
-                    });
-
-                    var ratios = [],
-                        currently_drawn = 0;
-
-                    svg.selectAll(".centroid")
-                        .each(function (d) {
-                            currently_drawn += d.count;
-                        })
-                        .each(function (d) {
-                            if (d.population > 0) {
-                                ratios.push((d.count/d.population)/currently_drawn);
-                            }else{
-                                ratios.push(0);
-                            }
-                        });
-
-                    var R = d3.scale.linear()
-                            .domain([0, d3.max(ratios)])
-                            .range([0, 20]);
-
-                    svg.selectAll(".centroid")
-                        .transition()
-                        .duration(500)
-                        .attr("r", function (d) {
-                            if (d.population < 1) {
-                                return 0;
-                            }
-                            return R((d.count/d.population)/currently_drawn);
-                        })
-                        .ease(d3.ease('elastic-in'));
-
-                    // svg.selectAll("g.centroids, g.points")
-                    //     .sort(function (a, b) {
-                    //         if (a.type == "centroids") return 1;
-                    //         return -1;
-                    //     });
-
-                 //   counter += to_draw.pos.length;//drawn.size();
-                 //   previous = now;
-
-
-//                    return counter >= ufos_in_batch;
-//                };
-//            })());
-        },
-
-        remove_ufos: function (ufos) {
-            if (!ufos) return;
-
-            var selector = ufos.map(function (ufo) {
-                return "#centroid-"+ufo.cluster;
-            }).join(", ");
-
-            var centroids = d3.selectAll(selector);
-            
-            centroids.each(function (d) {
-                d.count -= 1;
-                d3.select(this).datum(d);
-            });
-
-            var ratios = [],
-                currently_drawn = 0;
-
-            svg.selectAll(".centroid")
-                .each(function (d) {
-                    currently_drawn += d.count;
-                })
-                .each(function (d) {
-                    if (d.population > 0) {
-                        ratios.push((d.count/d.population)/currently_drawn);
-                    }else{
-                        ratios.push(0);
-                    }
-                });
-
-            var R = d3.scale.linear()
-                    .domain([0, d3.max(ratios)])
-                    .range([0, 20]);
-
-            svg.selectAll(".centroid")
-                .transition()
-                .duration(500)
-                .attr("r", function (d) {
-                    if (d.population < 1) {
-                        return 0;
-                    }
-                    return R((d.count/d.population)/currently_drawn);
-                })
-                .ease(d3.ease('elastic-out'));
         },
 
         draw_keyframe: (function () {
@@ -284,12 +105,6 @@ var Drawers = function (svg, ufos, populations, geo_path, geo_projection) {
                     .transition()
                     .duration(250)
                     .style("opacity", .3);
-
-                // svg.selectAll("g.centroids, .point")
-                //     .sort(function (a, b) {
-                //         if (a.type == "centroids") return 1;
-                //         return -1;
-                //     });
             };
         })(),
         
