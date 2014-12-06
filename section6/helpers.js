6daeb1941fbb647289852b6b7e1343048274e1f9
Swizec Teller <swizec@swizec.com>
2014-12-06 21:40:44 +0100

    indentation

--- section6/helpers.js
+++ section6/helpers.js
@@ -304,7 +304,7 @@ var make_keyframe = function make_keyframe (ufos_by_season, geo_projection,
     });
 
     var positions = ufos
-            .map(function (ufo) {
+                        .map(function (ufo) {
                 return geo_projection([Number(ufo.lon), Number(ufo.lat)]);
             })
             .map(function (pos, i) {
