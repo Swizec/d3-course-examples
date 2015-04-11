61ffcf7038fed45fbb8e2bd9c92d6dd97283d9a3
Swizec Teller <swizec@swizec.com>
2015-04-11 07:56:01 -0700

    state at end of videos for section3

--- section3/code.js
+++ section3/code.js
@@ -4,25 +4,20 @@
     var slugify = function (string) {
         return string
             .toLowerCase()
-            .replace(/ /g,'-')
-            .replace(/[^\w-]+/g,'-');
-    };
-
-    var time_taken = function (since) {
-        return (((new Date())-since)/1000)+"s";
+            .replace(/ /g, '-')
+            .replace(/[^\w-]+/g, '-');
     };
 
     var parse_index = function (fragment) {
         return Array.prototype.map.call(
-	    fragment.querySelectorAll("tbody tr"),
-	    function (row) {
-	        var a = row.querySelector("a");
-                
+            fragment.querySelectorAll("tbody tr"),
+            function (row) {
+                var a = row.querySelector("a");
+
                 return {state: a.textContent,
                         count: Number(row.querySelectorAll("td")[1].textContent),
                         link: "data/"+slugify(a.textContent)+".html"};
-                
-	    });
+            });
     };
 
     var fetch_a_state = function (state, callback) {
@@ -38,33 +33,12 @@
             state.data = Array.prototype.map.call(
                 fragment.querySelectorAll("tbody tr"),
                 function (row) {
-                    var cells = row.querySelectorAll("td"),
-                        parsed = {};
-
-                    ["time", "city", "state", "shape", 
-                     "duration", "summary", "posted"].forEach(
-                        function (key, i) {
-                            parsed[key] = cells[i].textContent;
-                        });
-                    
-                    parsed.time = time_formatter.parse(parsed.time);
-                    parsed.posted = posted_formatter.parse(parsed.posted);
-
-                    return parsed;
+                
                 })
                 .filter(function (datum) {
-                    return !!datum.time && !!datum.posted;
                 })
                 .map(function (datum) {
-                    // fix y2k problems
-                    if (datum.time.getFullYear() > this_year) {
-                        datum.time.setFullYear(datum.time.getFullYear()-100);
-                    }
-                    if (datum.posted.getFullYear() > this_year) {
-                        datum.posted.setFullYear(datum.posted.getFullYear()-100);
-                    }
-
-                    return datum;
+                
                 });
 
             callback(null, state);
@@ -72,39 +46,29 @@
     };
 
     d3.html("state-index.html", function (fragment) {
-        var index = parse_index(fragment),
-            start = new Date();
+        var index = parse_index(fragment);
 
         async.map(index, fetch_a_state, function (err, data) {
             var time_formatter = d3.time.format("%x %H:%M"),
                 posted_formatter = d3.time.format("%x");
 
-            console.log(data.length);
-            console.log(time_taken(start));
-
-            start = new Date();
-            
             data = data.map(function (datum) {
                 return datum.data.map(function (datum) {
-                    
                     datum.time = time_formatter(datum.time);
                     datum.posted = posted_formatter(datum.posted);
-                    
+
                     return datum;
                 });
             }).reduce(function (a, b) {
                 return a.concat(b);
             });
 
-            console.log("reshaped data for csv", time_taken(start));
-
             var csv = d3.csv.format(data);
-            
+
             var blob = new Blob([csv], {type: "text/plain;charset=utf-8"});
-            
-            console.log("got csv", time_taken(start));
 
             saveAs(blob, "full-data.csv");
         });
     });
+
 })();
--- section3/scrape.py
+++ section3/scrape.py
@@ -4,28 +4,6 @@ from slugify import slugify
 
 import urllib, os, urllib2
 
-def download(url, file_name):
-    u = urllib2.urlopen(url)
-    f = open(file_name, 'wb')
-    meta = u.info()
-    file_size = int(meta.getheaders("Content-Length")[0])
-    print "Downloading: %s Bytes: %s" % (file_name, file_size)
-
-    file_size_dl = 0
-    block_sz = 8192
-    while True:
-        buffer = u.read(block_sz)
-        if not buffer:
-            break
-
-        file_size_dl += len(buffer)
-        f.write(buffer)
-        status = r"%10d  [%3.2f%%]" % (file_size_dl, file_size_dl * 100. / file_size)
-        status = status + chr(8)*(len(status)+1)
-        print status,
-
-    f.close()
-
 def scrape():
     d = pq(filename="state-index.html")
 
@@ -38,8 +16,7 @@ def scrape():
         file = "./data/%s.html" % slugify(state)
 
         if not os.path.exists(file):
-            download(url, file)
-            #urllib.urlretrieve(url, file)
+            urllib.urlretrieve(url, file)
         else:
             print "Skip"
 
