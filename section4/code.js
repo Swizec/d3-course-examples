94bcc45f38eb1e51a5f51ddb7786ebaf1bc107fe
Swizec Teller <swizec@swizec.com>
2015-04-11 10:15:25 -0700

    state at end of 04_01 video

--- section4/code.js
+++ section4/code.js
@@ -4,15 +4,16 @@
     var width = 960,
         height = 600;
 
-    var projection = d3.geo.albersUsa()
-            .scale(1280)
-            .translate([width / 2, height / 2]),
-        path = d3.geo.path()
-            .projection(projection);
-
-    var svg = d3.select("#graph").append("svg")
+    var svg = d3.select("#graph")
+            .append("svg")
             .attr("width", width)
             .attr("height", height);
+
+    var projection = d3.geo.albersUsa()
+            .scale(1280)
+            .translate([width/2, height/2]),
+        path = d3.geo.path()
+            .projection(projection);
 
     var stateIdMap = d3.map({
         1: "AL",
@@ -70,27 +71,20 @@
 
     queue()
         .defer(d3.json, "us.json")
-        .defer(d3.csv, "full-data.csv")
-        .await(function (err, US, ufos) {
-            ufos = _.groupBy(ufos.filter(function (ufo) { return !!ufo.state; }),
-                             function (ufo) { return ufo.state; });
-
+        .await(function (err, US) {
             var states = svg.append("g")
                     .attr("class", "states")
                     .selectAll("g")
                     .data(topojson.feature(US, US.objects.states).features)
-                    .enter().append("g");
+                    .enter()
+                    .append("g");
 
             states.append("path")
-                .attr("d", path)
-                .on("click", function (d) {
-                    console.log(d, d.id, path.centroid(d));
-                });
+                .attr("d", path);
 
-            
-            svg.append("path")
-                .datum(topojson.mesh(US, US.objects.states, 
-                                     function(a, b) { return a !== b; }))
+            states.append("path")
+                .datum(topojson.mesh(US, US.objects.states,
+                                     function (a, b) { return a !== b; }))
                 .attr("class", "borders")
                 .attr("d", path);
 
@@ -100,6 +94,6 @@
                     x: function (d) { return path.centroid(d)[0] || 0; },
                     y: function (d) { return path.centroid(d)[1] || 0; }
                 });
+            
         });
-
 })();
